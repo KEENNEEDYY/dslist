da770709faed959fe0a5195a9192a646fc88e041
KENNEDY COSTA <kennedy.costa.valenet@gmail.com>
2023-05-09 21:15:32 -0300

    Game repository, service, controller

---
+++ src/main/java/com/desenvolvedorkennedy/dslist/dto/GameMinDTO.java
@@ -0,0 +1,57 @@
+package com.desenvolvedorkennedy.dslist.dto;
+
+import com.desenvolvedorkennedy.dslist.entities.Game;
+
+public class GameMinDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+
+    public GameMinDTO() {
+    }
+
+    public GameMinDTO(Long id, String title, Integer year, String imgUrl, String shortDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+    }
+
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+    public Long getId() {
+        return this.id;
+    } 
+
+    public String getTitle() {
+        return this.title;
+    } 
+
+    public Integer getYear() {
+        return this.year;
+    } 
+
+    public String getImgUrl() {
+        return this.imgUrl;
+    } 
+
+    public String getShortDescription() {
+        return this.shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+}
